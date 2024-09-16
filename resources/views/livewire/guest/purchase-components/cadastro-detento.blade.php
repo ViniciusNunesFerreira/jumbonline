fe27341c4d64eaa867a375c18fa14173a0ed2cf7
ViniciusNunesFerreira <viniciusnunes_ads@hotmail.com>
2024-09-15 22:17:53 -0300

    add webhook MP

--- resources/views/livewire/guest/purchase-components/cadastro-detento.blade.php
+++ resources/views/livewire/guest/purchase-components/cadastro-detento.blade.php
@@ -1,12 +1,13 @@
 <div>
-    <div class="mx-auto max-w-7xl py-8  space-y-4">
-
+    
     <form
         wire:submit.prevent="saveData"
         class="space-y-6"
 
         :wire:key="time()"
     >
+    <div class="mx-auto max-w-7xl py-8  space-y-4">
+        
         <x-card>
             <x-slot:content class="!py-8 sm:!px-10">
                 <h2 class="text-lg font-semibold text-purple tracking-tight flex items-center"> 
@@ -323,7 +324,8 @@
             </x-slot:content>
         </x-card>
 
+    </div>
+
     </form>
 
-    </div>
 </div>
