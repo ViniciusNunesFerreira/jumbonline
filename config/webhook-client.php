91dd0729604b218bb1bf78feb1b7374ebbc2fa98
ViniciusNunesFerreira <viniciusnunes_ads@hotmail.com>
2024-09-16 00:28:30 -0300

    add webhook MP

--- config/webhook-client.php
+++ config/webhook-client.php
@@ -11,7 +11,7 @@ return [
             'webhook_profile' => \Spatie\WebhookClient\WebhookProfile\ProcessEverythingWebhookProfile::class,
             'webhook_response' => \Spatie\WebhookClient\WebhookResponse\DefaultRespondsTo::class,
             'webhook_model' => \Spatie\WebhookClient\Models\WebhookCall::class,
-            'process_webhook_job' => \App\Jobs\MercadopagoWebhooks\ProcessMercadoPagoWebhookJob::class,
+            'process_webhook_job' => App\Jobs\MercadopagoWebhooks\ProcessMercadoPagoWebhookJob::class,
         ],
     ],
 
