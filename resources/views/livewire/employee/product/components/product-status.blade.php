25cab359c2822f783101e99ad87e6cb49278beb3
Marcos Vinicius Nunes Ferreira <viniciusnunes_ads@hotmail.com>
2026-08-06 22:12:49 -0300

    modify sales_type

--- resources/views/livewire/employee/product/components/product-status.blade.php
+++ resources/views/livewire/employee/product/components/product-status.blade.php
@@ -102,10 +102,10 @@
                 </div>
 
                 <div class="mt-2">
-                    
+
                     <x-input-label> Canal de Vendas </x-input-label>
                         <x-select
-                            x-on:change="$nextTick(() => $el.value !== '{{ product.sales_channel }}' ? dirty.add('sales_channel') : dirty.delete('sales_channel'))"
+                            x-on:change="$nextTick(() => $el.value !== '{{ $product.sales_channel }}' ? dirty.add('sales_channel') : dirty.delete('sales_channel'))"
                             wire:model="product.sales_channel"
                             class="sm:text-sm mt-2"
                         >
