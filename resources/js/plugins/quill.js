6db57fcccdb2b71f5a12a24b55a16119516ad616
Marcos Vinicius Nunes Ferreira <viniciusnunes_ads@hotmail.com>
2026-09-01 04:07:38 -0300

    correções editor content blog

--- resources/js/plugins/quill.js
+++ resources/js/plugins/quill.js
@@ -1,8 +1,4 @@
 import quill from 'quill/dist/quill.js';
 import 'quill/dist/quill.snow.css';
-import QuillBetterTablePlus from 'quill-better-table-plus';
-import 'quill-better-table-plus/dist/quill-better-table-plus.css';
-
-quill.register({ 'modules/better-table-plus': QuillBetterTablePlus }, true);
 
 window.Quill = quill;
--- resources/views/components/quill.blade.php
+++ resources/views/components/quill.blade.php
@@ -1,21 +1,16 @@
 <div
-    x-data="{ content: @entangle($attributes->wire('model')).defer,  quill: null}"
+    x-data="{ content: @entangle($attributes->wire('model')).defer }"
     x-ref="quillEditor"
     x-model="content"
     x-init="
         function setupEditor() {
             quill = new Quill($refs.quillEditor, {
                 modules: {
-                    table: false,
-                    'better-table-plus': {},
-                    keyboard: { bindings: window.QuillBetterTablePlus?.keyboardBindings },
                     toolbar: [
-                        [{ 'header': [2, 3, false] }],
                         ['bold', 'italic', 'underline'],
                         [{ 'list': 'ordered' }, { 'list': 'bullet' }],
-                        ['blockquote'],
-                        ['link'],
-                    ],
+                        ['link']
+                    ]
                 },
                 placeholder: '{{ __('Write something...') }}',
                 theme: 'snow'
@@ -24,17 +19,6 @@
             quill.on('text-change', function () {
                $dispatch('input', quill.root.innerHTML);
             });
-            $data.quill = quill;
         }
     "
->
-    <div class="mb-2 flex justify-end">
-        <button
-            type="button"
-            x-on:click="quill.getModule('better-table-plus').insertTable(3, 3)"
-            class="text-xs font-semibold text-purple hover:text-accent"
-        >
-            + Inserir tabela
-        </button>
-    </div>
-</div>
+></div>
